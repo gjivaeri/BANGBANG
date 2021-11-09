3325ca8273965060ce26deaf363dacf85dfa6ca4
gjivaeri <gjivaeri@gmail.com>
2021-11-09 22:34:15 +0900

    Fix: new_themeRev css link

--- BANGBANG/BANGPRO/BANGAPP/static/js/like.js
+++ BANGBANG/BANGPRO/BANGAPP/static/js/like.js
@@ -5,7 +5,7 @@ $(".like").click(function () { // .like 버튼을 클릭 감지
       type: "POST", url: '/like/', data: { 'pk': pk, 'csrfmiddlewaretoken': '{{ csrf_token }}' }, dataType: "json", 
       success: function (response) { // 성공
           // alert(response.message);
-          $("#count-" + pk).html(response.like_count + "+"); // 좋아요 개수 변경
+          $("#count-" + pk).html("♡" + response.like_count + "+"); // 좋아요 개수 변경
       },
       error: function (request, status, error) { // 실패
           alert("로그인이 필요합니다.");
