6ac83494ac2752fe4565fdd6ed3680c898e66d75
suyeoni4731 <https4731@naver.com>
2021-11-20 01:23:06 +0900

    test1

--- BANGBANG/BANGPRO/BANGAPP/views.py
+++ BANGBANG/BANGPRO/BANGAPP/views.py
@@ -116,17 +116,19 @@ def home(request):
     username = request.session.get('user')
     user = User.objects.filter(userID = username).values('userID')
     themes = Theme.objects.all()
-    count = themes.count()
-    content = {'user' : user, 'themes' : themes, 'count' : count}
+    shops = Shop.objects.all()
+    themescount = themes.count()
+    shopscount = shops.count()
+    content = {'user' : user, 'themes' : themes, 'shops' : shops, 'themescount' : themescount, 'shopscount' : shopscount}
     return render(request, 'home.html', content)
 
+def detail_shop(request, shop_pk):
+      shop = Shop.objects.get(pk=shop_pk)
+      themes = Theme.objects.filter(ShopID=shop_pk)
+      review = ThemeRev.objects.filter(theme_ID=shop_pk)
+      topreview = review.order_by('-themeRevRecom').first()
 
-def shop(request, shop_pk):
-    shop = Shop.objects.get(pk=shop_pk)
-    reviews = Shoprev.objects.filter(shopID=shop_pk)
-
-    return render(request,'shop.html',{'shop':shop, 'reviews':reviews})
-
+      return render(request,'detail_shop.html', {'shop':shop, 'themes':themes, 'review':review, 'topreview':topreview})
 
 def detail_theme(request, theme_pk):
   #이 페이지에서 새로 뭘 작성할게 아니면 아래 두줄은 삭제
