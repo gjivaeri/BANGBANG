674b7c4c4b06c9a63295d4b2d6346d5b5b86ef94
gjivaeri <gjivaeri@gmail.com>
2021-12-20 19:21:48 +0900

    test2

--- BANGPRO/settings.py
+++ BANGPRO/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 from django.core.exceptions import ImproperlyConfigured
 import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
 # Quick-start development settings - unsuitable for production
